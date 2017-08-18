1235425a390844081213382a05e7c825bad36f7f
Irfan Uraizee <iuraizee@gmail.com>
2017-08-18 10:11:34 -0400

    jquery

--- gulpfile.js/task-config.js
+++ gulpfile.js/task-config.js
@@ -12,11 +12,7 @@ module.exports = {
       // files paths are relative to
       // javascripts.dest in path-config.json
       app: ["./app.js"]
-    },
-    provide: {
-      $: "jquery",
-      jQuery: "jquery"
-    },
+    }
   },
 
   browserSync: {
